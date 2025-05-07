04dbb1573ac3ad1b64399fedd9cff950e795889b
Jeffrey2659 <116681319+Jeffrey2659@users.noreply.github.com>
2025-05-07 11:54:19 -0400

    added key movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,19 +29,26 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x +=1
-                    self.ship.moving_right =True 
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-            elif event.key 
+                self._check_keyup_events(event)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
-
-    
+    def _check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.rect.x +=1
+            self.ship.moving_right =True 
+        elif event.key  == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    def _check_keyup_events(self,event):
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key  == pygame.K_LEFT:
+                self.ship.moving_left = False
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -9,14 +9,23 @@ class Ship:
         #get dim of ship
         self.rect = self.image.get_rect()
         #starts ship atht eh bottom center of the scrren
+      
+     
+
+        self.settings = ai_game.settings
         self.rect.midbottom =  self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
         self.moving_right = False 
         self.moving_left = False
+
     def update(self):
-        if self.moving_right:
-            self.rect.x +=1
-        if self.moving_left:
-            self.rect.x -=1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
     def blitme(self):
         #draw the ship at its current location 
         self.screen.blit(self.image,self.rect)
